4c234628f85cf6c25d21833f5dadda00edc9c361
shrutibachale <shrutibachale7@gmail.com>
2025-12-24 15:24:17 +0530

    portfolio updated

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -14,13 +14,13 @@ const ContactMe = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_4mcpbhm",
-        "template_3xdndli",
-        form.current,
-        "0BNIQq-DfchUzb_Hx"
-      )
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      form.current,
+      process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+    )
+
       .then(
         () => {
           setIsSent(true);
